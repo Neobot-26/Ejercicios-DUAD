48498231cd5bd2f81e0d43731c9543ee724b2bf6
Didier <valverded@yahoo.com>
2026-06-03 20:43:54 -0600

    Bubble sorting algorithms

---
+++ 4 Python Intermedio/10Ordenamiento/BubbleSort_MintoMax.py
@@ -0,0 +1,12 @@
+def bubble_sort(list):
+    for index in range(len(list)-1):
+        for index2 in range(len(list)-1-index):
+            if list[index2]>list[index2+1]:
+                temporary=list[index2]
+                list[index2]=list[index2+1]
+                list[index2+1]=temporary
+    return list
+
+#Main
+list=[100,5,46,1,24,73,12,31]
+print(bubble_sort(list))
